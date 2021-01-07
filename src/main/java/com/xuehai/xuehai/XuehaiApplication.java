e9c4603323fc72b829a68c29e94d7184fca9976b
Thad-Yo <736602265@qq.com>
2021-01-07 17:30:19 +0800

    springboot

---
+++ src/main/java/com/xuehai/xuehai/XuehaiApplication.java
@@ -0,0 +1,13 @@
+package com.xuehai.xuehai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XuehaiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XuehaiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xuehai/xuehai/XuehaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xuehai.xuehai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XuehaiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
